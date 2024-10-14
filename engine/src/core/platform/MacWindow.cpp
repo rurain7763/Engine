8fe4c1aba0a650ac529cb4c9d5508c8a764b8fc1
rurain7763 <rurain7763@gmail.com>
2024-10-14 18:12:00 +0900

    mac window event 세팅

--- engine/src/core/platform/MacWindow.cpp
+++ engine/src/core/platform/MacWindow.cpp
@@ -3,7 +3,15 @@
 #ifdef PLATFORM_MAC
 #include "../DisplayWindow.h"
 #include "../Logger.h"
+
+#include "../events/WindowClosedEvent.h"
+#include "../events/WindowResizedEvent.h"
 #include "../events/KeyPressedEvent.h"
+#include "../events/KeyReleasedEvent.h"
+#include "../events/MouseButtonPressedEvent.h"
+#include "../events/MouseButtonReleasedEvent.h"
+#include "../events/MouseMovedEvent.h"
+#include "../events/MouseScrolledEvent.h"
 
 namespace engine {
 	DisplayWindow::DisplayWindow() {
@@ -26,11 +34,62 @@ namespace engine {
 		glfwMakeContextCurrent(window);
 
 		_window = window;
+		_width = width;
+		_height = height;
+
+		glfwSetWindowUserPointer(window, this);
+
+		glfwSetWindowCloseCallback(window, [](GLFWwindow* window) {
+			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
+			displayWindow->GetEventBus()->Publish<WindowClosedEvent>();
+		});
+
+		glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
+			displayWindow->GetEventBus()->Publish<WindowResizedEvent>(width, height);
+		});
 
 		glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
-			DisplayWindow* diplayWindow = (DisplayWindow*)window;
-			EG_ASSERT(diplayWindow->GetEventBus() != nullptr, "DisplayWindow is null");
-			diplayWindow->GetEventBus()->Publish<KeyPressedEvent>(key);
+			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
+			switch(action) {
+				case GLFW_PRESS:
+					displayWindow->GetEventBus()->Publish<KeyPressedEvent>(key);
+					break;
+				case GLFW_RELEASE:
+					displayWindow->GetEventBus()->Publish<KeyReleasedEvent>(key);
+					break;
+			}
+		});
+
+		glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
+			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
+
+			int key = button;
+			if(button == GLFW_MOUSE_BUTTON_LEFT) key = GLFW_MOUSE_BUTTON_LEFT;
+			else if(button == GLFW_MOUSE_BUTTON_RIGHT) key = GLFW_MOUSE_BUTTON_RIGHT;
+			else if(button == GLFW_MOUSE_BUTTON_MIDDLE) key = GLFW_MOUSE_BUTTON_MIDDLE;
+
+			switch(action) {
+				case GLFW_PRESS:
+					displayWindow->GetEventBus()->Publish<MouseButtonPressedEvent>(key);
+					break;
+				case GLFW_RELEASE:
+					displayWindow->GetEventBus()->Publish<MouseButtonReleasedEvent>(key);
+					break;
+			}
+		});
+
+		glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) {
+			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
+			displayWindow->GetEventBus()->Publish<MouseMovedEvent>(xpos, ypos);
+		});
+
+		glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) {
+			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
+			displayWindow->GetEventBus()->Publish<MouseScrolledEvent>(
+				static_cast<float>(xoffset), 
+				static_cast<float>(yoffset)
+			);
 		});
 	}
 
@@ -40,6 +99,7 @@ namespace engine {
 		glfwTerminate();
 	}
 }
+
 
 #endif
 
