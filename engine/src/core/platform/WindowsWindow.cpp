86e4168aa391b931c7310e06fc1508d8dd16aed3
rurain7763 <rurain7763@gmail.com>
2024-10-17 23:18:26 +0900

    windows 세팅

--- engine/src/core/platform/WindowsWindow.cpp
+++ engine/src/core/platform/WindowsWindow.cpp
@@ -2,6 +2,10 @@
 #include "../DisplayWindow.h"
 #include "../Logger.h"
 
+#include "../DisplayWindow.h"
+#include "../Logger.h"
+
+#include "eventbus/EventBus.h"
 #include "../events/WindowClosedEvent.h"
 #include "../events/WindowResizedEvent.h"
 #include "../events/KeyPressedEvent.h"
@@ -11,151 +15,85 @@
 #include "../events/MouseMovedEvent.h"
 #include "../events/MouseScrolledEvent.h"
 
-#include "../layers/ImGuiLayer.h"
-#include "graphics/GraphicsContext.h"
-#include "graphics/Shader.h"
-#include "graphics/Buffer.h"
-#include "graphics/VertexArray.h"
-
-#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 namespace engine {
 	DisplayWindow::DisplayWindow() {
 		_eventBus = std::make_unique<EventBus>();
-		_layerGroup = std::make_unique<LayerGroup>();
 	}
 
 	void DisplayWindow::Init(int width, int height, const char* title) {
 		EG_ASSERT(glfwInit() == GLFW_TRUE, "Failed to initialize GLFW");
 
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
 		GLFWwindow* window = glfwCreateWindow(width, height, title, nullptr, nullptr);
 		EG_ASSERT(window != nullptr, "Failed to create window");
 
-		_graphicsContext = GraphicsContext::Create(GraphicsAPI::OpenGL, window);
-		_graphicsContext->Init();
-
 		glfwMakeContextCurrent(window);
-
-		_window = window;
-		_width = width;
-		_height = height;
 
 		glfwSetWindowUserPointer(window, this);
 
 		glfwSetWindowCloseCallback(window, [](GLFWwindow* window) {
 			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
 			displayWindow->GetEventBus()->Publish<WindowClosedEvent>();
-		});
+			});
 
 		glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
 			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
 			displayWindow->GetEventBus()->Publish<WindowResizedEvent>(width, height);
-		});
+			});
 
 		glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
 			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
-			switch(action) {
-				case GLFW_PRESS:
-					displayWindow->GetEventBus()->Publish<KeyPressedEvent>(key);
-					break;
-				case GLFW_RELEASE:
-					displayWindow->GetEventBus()->Publish<KeyReleasedEvent>(key);
-					break;
+			switch (action) {
+			case GLFW_PRESS:
+				displayWindow->GetEventBus()->Publish<KeyPressedEvent>(key);
+				break;
+			case GLFW_RELEASE:
+				displayWindow->GetEventBus()->Publish<KeyReleasedEvent>(key);
+				break;
 			}
-		});
+			});
 
 		glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
 			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
-			switch(action) {
-				case GLFW_PRESS:
-					displayWindow->GetEventBus()->Publish<MouseButtonPressedEvent>(button);
-					break;
-				case GLFW_RELEASE:
-					displayWindow->GetEventBus()->Publish<MouseButtonReleasedEvent>(button);
-					break;
+			switch (action) {
+			case GLFW_PRESS:
+				displayWindow->GetEventBus()->Publish<MouseButtonPressedEvent>(button);
+				break;
+			case GLFW_RELEASE:
+				displayWindow->GetEventBus()->Publish<MouseButtonReleasedEvent>(button);
+				break;
 			}
-		});
+			});
 
 		glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) {
 			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
 			displayWindow->GetEventBus()->Publish<MouseMovedEvent>(static_cast<int>(xpos), static_cast<int>(ypos));
-		});
+			});
 
 		glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) {
 			DisplayWindow* displayWindow = (DisplayWindow*)glfwGetWindowUserPointer(window);
 			displayWindow->GetEventBus()->Publish<MouseScrolledEvent>(xoffset, yoffset);
-		});
-	
-		_vertexArray.reset(VertexArray::Create());
-		_vertexArray->Bind();
+			});
 
-		float vertices[] = {
-			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
-			 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-			 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f
-		};
-
-		_vertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices) / sizeof(float)));
-
-		VertexBufferLayout layout;
-		layout.Push<float>("aPos", 3);
-		layout.Push<float>("aColor", 4);
-
-		_vertexBuffer->SetLayout(layout);
-		_vertexArray->AddVertexBuffer(_vertexBuffer);
-
-		unsigned int indices[3] = { 0, 1, 2 };
-		_indexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(unsigned int)));
-		_vertexArray->SetIndexBuffer(_indexBuffer);
-
-		const char* vertexShaderSource = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 aPos;
-			layout(location = 1) in vec4 aColor;
-			layout(location = 2) in vec2 aTexCoord;
-
-			out vec4 vertexColor;
-
-			void main() {
-				gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-				vertexColor = aColor;
-			}
-		)";
-
-		const char* fragmentShaderSource = R"(
-			#version 330 core
-
-			out vec4 FragColor;
-
-			in vec4 vertexColor;
-
-			void main() {
-				FragColor = vec4(vertexColor);
-			}
-		)";
-
-		_shader = std::make_unique<Shader>(vertexShaderSource, fragmentShaderSource);
+		_window = window;
+		_width = width;
+		_height = height;
 	}
 
-	void DisplayWindow::Render() {
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		_shader->Bind();
-		_vertexArray->Bind();
-		glDrawElements(GL_TRIANGLES, _vertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
-
-		_layerGroup->Render();
-
-		_graphicsContext->SwapBuffers();
+	void DisplayWindow::Update() {
 		glfwPollEvents();
 	}
 
 	void DisplayWindow::Destroy() {
 		GLFWwindow* window = (GLFWwindow*)_window;
 
-		delete _graphicsContext;
 		glfwDestroyWindow(window);
 		glfwTerminate();
 	}
